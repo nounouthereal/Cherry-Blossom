2293150e7901c0e94895d463469cfc054faaf77f
nounouthereal <nouhame.pasquier@gmail.com>
2022-08-30 08:45:00 +0100

    See if gitignore worked

--- Js/bot/slashCommands/economy/rob.js
+++ Js/bot/slashCommands/economy/rob.js
@@ -2,7 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     name: "rob",
-    description: "",
+    description: "lll",
     timeout: 5000,
     options: [
 
@@ -18,7 +18,172 @@ module.exports = {
 
     run: async (bot, interaction, args) => {
 
+        const usertag = message.member;
+        const user = await bot.fetchUser(message.author.id);
+        const member = message.mentions.members.first() || message.guild.members.cache.get(args.join(' ')) || message.guild.members.cache.find(member => member.user.username.toLowerCase() === args.join(' ').toString().toLowerCase());
+    
+    
+        let passivewarn = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`❌ **${usertag.user.username}** : You have  \`PASSIVE\` enabled, you need to disable it to use this command.`);
+    
+        if (user.passive == true) return message.channel.send({embeds: [passivewarn]});
+
+        let authorwarn = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`❌ <@${usertag.user.id}> : You cannot rob yourself.`);
+    
+        if (member.id == message.author.id) return message.channel.send({embeds: [authorwarn]});
+    
+        if (!member) {
         
+        let rob1embed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`❌ <@${usertag.user.id}> : Vous avez oublié la personne que vous voulez voler.`);
+        return message.channel.send({embeds: [rob1embed]});
+        }
+
+        
+        const devs = ['901071562386583596'];
+
+        if (devs.includes(member.user.id)) {
+        
+        let rob2embed = new MessageEmbed()
+        .setColor("YELLOW")
+        .setDescription(`🛡 <@${usertag.user.id}> : You can't rob the bot devs`);
+        return message.channel.send({embeds: [rob2embed]});
+        }
+        
+        const robbedUser = await bot.fetchUser(member.id);
+        if (robbedUser.passive == true) {
+        
+        let rob3embed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(`❌ <@${usertag.user.id}> : The member you are trying to rob have passive: \`ENABLED\`.`);
+        return message.channel.send({embeds: [rob3embed]});
+        //return message.channel.send(`Leave them alone... they are in passive mode`);
+        }
+
+        if (robbedUser.coinsInWallet < 10000) {
+        let rob4embed = new MessageEmbed()
+        .setColor("BLUE")
+        .setDescription(`🛡 <@${usertag.user.id}> : The user you tried to steal has protections on their balance at the moment because they have a balance of less than \`10 000\` :coin:.`);
+        return message.channel.send({embeds: [rob4embed]});
+            //return message.channel.send("This user doesn't have much coins, I wouldn't rob them");
+        }
+
+        if (user.coinsInWallet < 1000) {
+            let rob4embed = new MessageEmbed()
+            .setColor("BLUE")
+            .setDescription(`🛡 <@${usertag.user.id}> : You must have at least balance of \`1000\` :coin:.`);
+            return message.channel.send({embeds: [rob4embed]});
+                //return message.channel.send("This user doesn't have much coins, I wouldn't rob them");
+        }
+
+        if (user.items.find(x => x.name == 'luckyclover')) {
+            const newInv = user.items.filter(i => i.itemId != 'luckyclover');
+            const bypass = user.items.find(i => i.itemId == 'luckyclover');
+
+            if (bypass.amount == 1) {
+                user.items = newInv;
+
+            } else {
+                newInv.push({ itemId: 'luckyclover', amount: bypass.amount - 1, description: bypass.description });
+                user.items = newInv
+            }
+
+        } else {
+            const random2 = Math.floor(Math.random() * 2);
+
+            const random_percent =  Math.floor(Math.random() * 10)
+            const randomAmount = Math.round(random_percent / 100 * robbedUser.coinsInWallet);
+            console.log(randomAmount)
+        
+
+            if (random2 === 2) {
+
+
+                user.coinsInWallet -= randomAmount 
+                robbedUser.coinsInWallet += randomAmount;
+
+                await user.save();
+                await robbedUser.save();
+
+
+                let rob5embed = new MessageEmbed()
+                .setColor("BLUE")
+                .setTitle(`💸 Robbery result:`)
+                .setDescription(`🛡 <@${usertag.user.id}> : You tried to steal <@${member.user.id}> and you got arrested 👮! You paid a bail of \`${randomAmount.toLocaleString()}\` :coin:.`)
+                .setFooter(`Asked by ${message.member.displayName} • ${message.guild.name}`,message.guild.iconURL())
+                .setTimestamp()
+
+                let robb7emb = new MessageEmbed()
+                .setColor("WHITE")
+                .setDescription(`🛡 <@${usertag.user.id}> : **${message.author.tag}** tried to rob you and was arrested 👮! He paid you a bail of \`${randomAmount.toLocaleString()}\` :coin:.`)
+                .setFooter(`Asked by ${message.member.displayName} • ${message.guild.name}`,message.guild.iconURL())
+                .setTimestamp()
+                message.robbedUser.send({embeds: [robb7emb]})
+                return message.channel.send({embeds: [rob5embed]});
+                //return message.channel.send(`You tried to rob **${member.user.tag}** but got caught👮! Better luck next time.`);
+            }
+
+            else if (random2 === 1) {
+
+                user.coinsInWallet += randomAmount;
+                robbedUser.coinsInWallet -= randomAmount;
+
+                await user.save();
+                await robbedUser.save();
+
+                let rob6embed = new MessageEmbed()
+                .setColor("GREEN")
+                .setDescription(`🥷 <@${usertag.user.id}> : You robbed \`${randomAmount.toLocaleString()}\` :coin: to <@${member.id}>!`)
+                .setFooter(`Asked by ${message.member.displayName} • ${message.guild.name}`,message.guild.iconURL())
+                .setTimestamp()
+                message.channel.send({embeds: [rob6embed]});
+
+                let rob7embed = new MessageEmbed()
+                .setColor("GREEN")
+                .setDescription(`🥷 <@${usertag.user.id}> : You had been robbed \`${randomAmount.toLocaleString()}\` :coin: by ${member.tag} !`)
+                .setFooter(`Asked by ${message.member.displayName} • ${message.guild.name}`,message.guild.iconURL())
+                .setTimestamp()
+                member.send({embeds: [rob7embed]});
+
+
+            }
+        }
+
+
+        let array = robbedUser.items.filter(x => x.itemId !== 'padlock');
+        const padlock = robbedUser.items.find(x => x.itemId === 'padlock');
+
+
+        if (padlock) { 
+            let rob6embed = new MessageEmbed()
+                .setColor("BLUE")
+                .setTitle(`💸 Robbery result:`)
+                .setDescription(`🛡 <@${usertag.user.id}> : You tried to steal **${member.user.tag}**, but he had a 🔒 **Padlock**. Try again next time.`)
+                .setFooter(`Asked by ${message.member.displayName} • ${message.guild.name}`,message.guild.iconURL())
+                .setTimestamp()
+                message.channel.send({embeds:[rob6embed]});
+        
+            if (padlock.amount === 1) {
+                robbedUser.items = array;
+                await robbedUser.save();
+                return;
+            }
+
+            else {
+                array.push({
+                    itemId: 'padlock',
+                    amount: padlock.amount - 1,
+                    description: padlock.description
+                });
+                robbedUser.items = array;
+                await robbedUser.save();
+                return;
+            }
+        }
 
     }
 }
--- Js/bot/utils/handlers/command.js
+++ Js/bot/utils/handlers/command.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const { glob } = require("glob");
+const { promisify } = require("util");
 
 module.exports = bot => {
     fs.readdir('/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/economy', (err, files) => {
@@ -76,6 +78,76 @@ module.exports = bot => {
         });
     });
 
+    fs.readdir('/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/utility', (err, files) => {
+        if (err) console.log(err);
+        const file = files.filter(f => f.split('.').pop() === 'js');
+        if (file.length < 1) {
+            return console.log('No Commands.');
+        }
+
+        file.forEach(f => {
+            const pull = require(`/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/utility/${f}`);
+            bot.commands.set(pull.config.name, pull);
+            console.log(`Commande ${pull.config.name} loaded`)
+            pull.config.aliases.forEach(alias => bot.aliases.set(alias, pull.config.name));
+        });
+    });
+
+    fs.readdir('/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/moderation', (err, files) => {
+        if (err) console.log(err);
+        const file = files.filter(f => f.split('.').pop() === 'js');
+        if (file.length < 1) {
+            return console.log('No Commands.');
+        }
+
+        file.forEach(f => {
+            const pull = require(`/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/moderation/${f}`);
+            bot.commands.set(pull.config.name, pull);
+            console.log(`Commande ${pull.config.name} loaded`)
+            pull.config.aliases.forEach(alias => bot.aliases.set(alias, pull.config.name));
+        });
+    });
+
+    fs.readdir('/Users/nouhame/Bot_des_cerisiers/Js/bot/commands', (err, dic, files) => {
+        if (err) console.log(err);
+        const file = files.filter(f => f.split('.').pop() === 'js');
+        if (file.length < 1) {
+            return console.log('No Commands.');
+        }
+
+        file.forEach(f => {
+            const pull = require(`/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/utility/${f}`);
+            bot.commands.set(pull.config.name, pull);
+            console.log(`Commande ${pull.config.name} loaded`)
+            pull.config.aliases.forEach(alias => bot.aliases.set(alias, pull.config.name));
+        });
+    });
+
+
+
+    const globPromise = promisify(glob);
+
+
+    const slashCommands = await globPromise(
+        `${process.cwd()}/commands/*/*.js`
+    );
+
+    slashCommands.map((value) => {
+        const file = require(value);
+        bot.commands.set(file.config.name, file);
+        console.log(`[Text] Command ${file?.config.name} loaded.`)
+        if (!file?.name) return;
+        bot.slashCommands.set(file.name, file);
+
+        arrayOfSlashCommands.push(file);
+    });
+    bot.on("ready", async () => {
+        bot.guilds.cache
+        .get("974767284855910410")
+        .commands.set(arrayOfSlashCommands);
+    })
+
+
 
     //Waiting for ranking system V1.0
     /* fs.readdir('/Users/nouhame/Bot_des_cerisiers/Js/bot/commands/ranking', (err, files) => {
@@ -92,4 +164,5 @@ module.exports = bot => {
             pull.config.aliases.forEach(alias => bot.aliases.set(alias, pull.config.name));
         });
     }); */
+
 }
